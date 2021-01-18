ab74da17c55f9c3ad557ea8246afba77811d07d7
Frogs114 <outskirts114@gmail.com>
2021-01-17 22:15:17 -0600

    Inital Release

---
+++ slackmain.py
@@ -0,0 +1,67 @@
+import slack
+import requests
+from bs4 import BeautifulSoup
+import time
+
+class UND:
+    def __init__(self):
+        global client
+        with open("slacktoken.txt", "r") as f:
+            token = f.read()
+        client = slack.WebClient(token=token)
+
+    def getFlightCategory(self):    # Takes the appropriate html text and sets it to a variable
+        flightCategoryClass = self.soup.find(class_="auto-style1b")
+        return flightCategoryClass.get_text()
+
+    def getRestrictions(self):  # Takes the appropriate html text and sets it to a variable
+        flightRestrictionsClass = self.soup.find(class_="auto-style4")
+        flightRestrictionsClass2 = self.soup.find(class_="auto-style5")
+        return flightRestrictionsClass.get_text(), flightRestrictionsClass2.get_text()
+
+    def scrape(self):
+        page = requests.get("http://sof.aero.und.edu")
+        self.soup = BeautifulSoup(page.content, "html.parser")
+
+    def getInfo(self):
+        return self.getFlightCategory(), self.getRestrictions()
+
+    def slackBot(self):
+        FC1 = 0
+        FR1 = 0
+        FR2 = 0
+        now = time.gmtime()
+        while 1 == 1:
+            if 1 == 1:  # now[6] <= 5 and 14 <= now[3] <= 23:   # if the day of the week is not sunday, and its between 23 - 14 Zulu (GMT)      8am to 5 CST
+                und.scrape()
+                flightCategory = und.getFlightCategory()
+                flightRestrictions1, flightRestrictions2 = und.getRestrictions()
+                if flightCategory == FC1 and flightRestrictions1 == FR1 and flightRestrictions2 == FR2:
+                    time1 = (now[3] - 8)
+                    print("No Change", time1)  # print time it checked for debugging
+                    time.sleep(300)
+                elif flightCategory != FC1 or flightRestrictions1 != FR1 or flightRestrictions2 != FR2:
+                    FC1 = flightCategory
+                    FR1 = flightRestrictions1
+                    FR2 = flightRestrictions2
+                    if flightRestrictions1 == "Manager on Duty:":
+                        client.chat_postMessage(channel="#sof",text=("<!channel> %s" % (flightCategory)))
+                        print("Posted, Closed")
+                        time.sleep(300)
+                    elif flightRestrictions2 == "Manager on Duty:":
+                        client.chat_postMessage(channel="#sof",text=("<!channel> %s, %s" % (flightCategory, flightRestrictions1)))
+                        time.sleep(300)
+                        print("Posted")
+                    elif flightRestrictions2 != "Manager on Duty:":
+                        client.chat_postMessage(channel="#sof", text=("<!channel> %s, %s, %s " % (flightCategory, flightRestrictions1, flightRestrictions2)))
+                        print("Posted")
+                        time.sleep(300)
+            else:
+                print("Outside Time")
+                time.sleep(300)
+
+# tm_year=2020, tm_mon=12, tm_mday=31, tm_hour=0, tm_min=37,
+# tm_sec=55, tm_wday=3, tm_yday=366, tm_isdst=0)
+
+und = UND()
+und.slackBot()
