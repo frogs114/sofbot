967da4bcc7ba7b05d2e904bce6bb18ce386c57d9
Frogs114 <outskirts114@gmail.com>
2021-01-18 19:40:14 -0600

    Added support for a 3rd flight restriction, fixed program run times.

--- discord.py
+++ discord.py
@@ -16,45 +16,48 @@ class UND:
     def getRestrictions(self):  # Takes the appropriate html text and sets it to a variable
         flightRestrictionsClass = self.soup.find(class_="auto-style4")
         flightRestrictionsClass2 = self.soup.find(class_="auto-style5")
-        return flightRestrictionsClass.get_text(), flightRestrictionsClass2.get_text()
+        flightRestrictionsClass3 = self.soup.find(class_="auto-style6")
+        return flightRestrictionsClass.get_text(), flightRestrictionsClass2.get_text(), flightRestrictionsClass3.get_text()
 
     def scrape(self):
         page = requests.get("http://sof.aero.und.edu")
         self.soup = BeautifulSoup(page.content, "html.parser")
 
-    def getInfo(self):
-        return self.getFlightCategory(), self.getRestrictions()
 
     def discordBot(self):
         FC1 = 0
         FR1 = 0
         FR2 = 0
+        FR3 = 0
         now = time.gmtime()
         while 1 == 1:
-            if 1 == 1:  # now[6] <= 5 and 14 <= now[3] <= 23:   # if the day of the week is not sunday, and its between 23 - 14 Zulu (GMT)      8am to 5 CST
+            if now[6] <= 5 and 3 <= now[3] <= 12:   # if the day of the week is not sunday, and its between 23 - 14 Zulu (GMT)      8am to 5 CST
                 und.scrape()
                 flightCategory = und.getFlightCategory()
-                flightRestrictions1, flightRestrictions2 = und.getRestrictions()
-                if flightCategory == FC1 and flightRestrictions1 == FR1 and flightRestrictions2 == FR2:
+                flightRestrictions1, flightRestrictions2, flightRestrictions3 = und.getRestrictions()
+                if flightCategory == FC1 and flightRestrictions1 == FR1 and flightRestrictions2 == FR2 and flightRestrictions3 == FR3:
                     time1 = (now[3] - 8)
                     print("No Change", time1)  # print time it checked for debugging
                     time.sleep(300)
-                elif flightCategory != FC1 or flightRestrictions1 != FR1 or flightRestrictions2 != FR2:
+                else:
                     FC1 = flightCategory
                     FR1 = flightRestrictions1
                     FR2 = flightRestrictions2
+                    FR3 = flightRestrictions3
                     if flightRestrictions1 == "Manager on Duty:":
                         DiscordWebhook(url=url, content=('<@&796776150578757662> %s' % flightCategory)).execute()
                         print("Posted, Closed")
                         time.sleep(300)
                     elif flightRestrictions2 == "Manager on Duty:":
-                        DiscordWebhook(url=url, content=(
-                                    '<@&796776150578757662> %s, %s' % (flightCategory, flightRestrictions1))).execute()
+                        DiscordWebhook(url=url, content=('<@&796776150578757662> %s, %s' % (flightCategory, flightRestrictions1))).execute()
                         time.sleep(300)
                         print("Posted")
-                    elif flightRestrictions2 != "Manager on Duty:":
-                        DiscordWebhook(url=url, content=('<@&796776150578757662> %s, %s, %s' % (
-                        flightCategory, flightRestrictions1, flightRestrictions2))).execute()
+                    elif flightRestrictions3 == "Manager on Duty:":
+                        DiscordWebhook(url=url, content=('<@&796776150578757662> %s, %s, %s' % (flightCategory, flightRestrictions1, flightRestrictions2))).execute()
+                        print("Posted")
+                        time.sleep(300)
+                    elif flightRestrictions3 != "Manager on Duty:":
+                        DiscordWebhook(url=url, content=('<@&796776150578757662> %s, %s, %s, %s' %(flightCategory, flightRestrictions1, flightRestrictions2, flightRestrictions3))).execute()
                         print("Posted")
                         time.sleep(300)
             else:
@@ -63,5 +66,4 @@ class UND:
 und = UND()
 und.discordBot()
 
-# tm_year=2020, tm_mon=12, tm_mday=31, tm_hour=0, tm_min=37,
-# tm_sec=55, tm_wday=3, tm_yday=366, tm_isdst=0)
+# time.struct_time(tm_year=2021, tm_mon=1, tm_mday=19, tm_hour=1, tm_min=15, tm_sec=14, tm_wday=1, tm_yday=19, tm_isdst=0)
--- slackmain.py
+++ slackmain.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
+
 class UND:
     def __init__(self):
         global client
@@ -17,51 +18,55 @@ class UND:
     def getRestrictions(self):  # Takes the appropriate html text and sets it to a variable
         flightRestrictionsClass = self.soup.find(class_="auto-style4")
         flightRestrictionsClass2 = self.soup.find(class_="auto-style5")
-        return flightRestrictionsClass.get_text(), flightRestrictionsClass2.get_text()
+        flightRestrictionsClass3 = self.soup.find(class_="auto-style6")
+        return flightRestrictionsClass.get_text(), flightRestrictionsClass2.get_text(), flightRestrictionsClass3.get_text()
 
     def scrape(self):
         page = requests.get("http://sof.aero.und.edu")
         self.soup = BeautifulSoup(page.content, "html.parser")
 
-    def getInfo(self):
-        return self.getFlightCategory(), self.getRestrictions()
 
     def slackBot(self):
         FC1 = 0
         FR1 = 0
         FR2 = 0
+        FR3 = 0
         now = time.gmtime()
         while 1 == 1:
-            if 1 == 1:  # now[6] <= 5 and 14 <= now[3] <= 23:   # if the day of the week is not sunday, and its between 23 - 14 Zulu (GMT)      8am to 5 CST
+            if now[6] <= 5 and 3 <= now[3] <= 12:   # if the day of the week is not sunday, and its between 23 - 14 Zulu (GMT)      8am to 5 CST
                 und.scrape()
                 flightCategory = und.getFlightCategory()
-                flightRestrictions1, flightRestrictions2 = und.getRestrictions()
-                if flightCategory == FC1 and flightRestrictions1 == FR1 and flightRestrictions2 == FR2:
+                flightRestrictions1, flightRestrictions2, flightRestrictions3 = und.getRestrictions()
+                if flightCategory == FC1 and flightRestrictions1 == FR1 and flightRestrictions2 == FR2 and flightRestrictions3 == FR3:
                     time1 = (now[3] - 8)
                     print("No Change", time1)  # print time it checked for debugging
                     time.sleep(300)
-                elif flightCategory != FC1 or flightRestrictions1 != FR1 or flightRestrictions2 != FR2:
+                else:
                     FC1 = flightCategory
                     FR1 = flightRestrictions1
                     FR2 = flightRestrictions2
+                    FR3 = flightRestrictions3
                     if flightRestrictions1 == "Manager on Duty:":
-                        client.chat_postMessage(channel="#sof",text=("<!channel> %s" % (flightCategory)))
+                        client.chat_postMessage(channel="#sof", text=("<!channel> %s" % flightCategory))
                         print("Posted, Closed")
                         time.sleep(300)
                     elif flightRestrictions2 == "Manager on Duty:":
                         client.chat_postMessage(channel="#sof",text=("<!channel> %s, %s" % (flightCategory, flightRestrictions1)))
                         time.sleep(300)
                         print("Posted")
-                    elif flightRestrictions2 != "Manager on Duty:":
+                    elif flightRestrictions3 == "Manager on Duty:":
                         client.chat_postMessage(channel="#sof", text=("<!channel> %s, %s, %s " % (flightCategory, flightRestrictions1, flightRestrictions2)))
+                        print("Posted")
+                        time.sleep(300)
+                    elif flightRestrictions3 != "Manager on Duty:":
+                        client.chat_postMessage(channel="#sof", text=("<!channel> %s, %s, %s , %s" % (flightCategory, flightRestrictions1, flightRestrictions2, flightRestrictions3)))
                         print("Posted")
                         time.sleep(300)
             else:
                 print("Outside Time")
                 time.sleep(300)
 
-# tm_year=2020, tm_mon=12, tm_mday=31, tm_hour=0, tm_min=37,
-# tm_sec=55, tm_wday=3, tm_yday=366, tm_isdst=0)
+# time.struct_time(tm_year=2021, tm_mon=1, tm_mday=19, tm_hour=1, tm_min=15, tm_sec=14, tm_wday=1, tm_yday=19, tm_isdst=0)
 
 und = UND()
 und.slackBot()
