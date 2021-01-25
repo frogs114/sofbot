4e77a42782a16aac392d0b4d3656ba29f88a00ad
Frogs114 <outskirts114@gmail.com>
2021-01-24 18:14:23 -0600

    Comments

--- discord.py
+++ discord.py
@@ -5,7 +5,7 @@ from discord_webhook import DiscordWebhook
 
 
 class UND:
-    def __init__(self):
+    def __init__(self):  # location of auth code for posting to discord
         global url
         with open("discordurl.txt", "r") as f:
             url = f.read()
@@ -26,7 +26,7 @@ class UND:
                 pass
         return restrictionslist
 
-    def setVariables(self, flightCategory, FRlist, restrictionslist2):
+    def setVariables(self, flightCategory, FRlist, restrictionslist2):  # creates memory variables.
         FC1 = flightCategory
         try:  # try to set variables equal
             FRlist[0] = restrictionslist2[0]
@@ -54,50 +54,48 @@ class UND:
             pass
         return FC1, FRlist
 
-    def scrape(self):  # sets up webpage desired
+    def scrape(self):  # sets up webpage desired and updates html text pulled
         page = requests.get("http://sof.aero.und.edu")
         self.soup = BeautifulSoup(page.content, "html.parser")
 
-    def discordBot(self):
-        FC1 = 0  # memory variables
-        now = time.gmtime()  # time variable for code execution
-        printlist = []
-        FRlist = []
-        und.scrape()  # gets info from page
+    def discordBot(self):  # main function to post scraped text to discord
+        FC1 = 0  # memory variable for flight category
+        printlist = []  # final list to be sent to discord
+        FRlist = []  # list for memory, to compare one iteration from the next
+        und.scrape()
         restrictionslist2 = und.getRestrictions()
-        for x in restrictionslist2:
+        for x in restrictionslist2:  # creates an initial list based on the length of flight restrictions to avoid an index error on inital run
             FRlist.append(0)
         while 1 == 1:
-            now = time.gmtime()
-            if now[6] <= 5 and (now[3] >= 12 or now[
-                3] <= 4):  # if the day of the week is not sunday, and its between 23 - 14 Zulu (GMT) 8am to 5 CST
-                und.scrape()  # gets info from page
+            now = time.gmtime()  # time variable for code execution
+            if now[6] <= 5 and (now[3] >= 12 or now[3] <= 4):  # if the day of the week is not sunday, and its between 23 - 14 Zulu (GMT) 8am to 5 CST
+                und.scrape()
                 flightCategory = und.getFlightCategory()  # sets html text to variables
                 restrictionslist2 = und.getRestrictions()
-                if flightCategory == FC1 and restrictionslist2 == FRlist:
+                if flightCategory == FC1 and restrictionslist2 == FRlist:  # if nothing has changed since the last pass
                     time1 = (now[3] - 6)
                     print("No Change", time1)  # print time it checked for debugging
                     time.sleep(300)
-                else:  # set memory variables
+                else:  # set memory variables for comparison later
                     FC1 = flightCategory
                     FRlist = restrictionslist2
                     #   FC1, FRlist = und.setVariables(flightCategory, FRlist, restrictionslist2)
-                    if flightCategory == 'Closed':
+                    if flightCategory == 'Closed':  # if the category is "Closed" only post closed
                         DiscordWebhook(url=url,
                                        content=('<@&796776150578757662> %s' % flightCategory)).execute()
                         print('Posted, Closed')
                         time.sleep(300)
-                    else:
+                    else:  # if not "closed" iterate through each attribute in list, until "Manager on Duty" appears
                         for attribute in restrictionslist2:
                             if attribute == "Manager on Duty:":
                                 string = ', '.join(printlist)
                                 DiscordWebhook(url=url, content=(
-                                            '<@&796776150578757662> %s, %s' % (flightCategory, string))).execute()
+                                        '<@&796776150578757662> %s, %s' % (flightCategory, string))).execute()
                                 print('Posted')
-                                printlist = []
+                                printlist = []  # reset the list to wipe all past restrictions
                                 time.sleep(300)
                                 break
-                            else:
+                            else:  # if not manager on duty, add the flight restriction to the list to be printed
                                 printlist.append(attribute)
 
             else:
